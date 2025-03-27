570125e9782e40b1558fd7522f0261bf2c43976e
elsomoose <113397833+elsomoose@users.noreply.github.com>
2025-03-27 17:59:30 +1100

    update button

--- Assignment2_3D/Assets/Adriel/Scripts/button.cs
+++ Assignment2_3D/Assets/Adriel/Scripts/button.cs
@@ -7,12 +7,13 @@ using UnityEngine.UI;
 
 public class button : MonoBehaviour
 {
-    
+    private float i = 0;
     public GameObject Cube;
 
     public void ButtonClicked()
     {
-
-        Object.Instantiate (Cube, new Vector3(0,3,0), Quaternion.identity);
+        Object.Instantiate (Cube, new Vector3(0,2,0), Quaternion.identity);
+        //Object.Instantiate (Cube, new Vector3(0,i,0), Quaternion.identity);
+        //i =+ 1.5f;
     }
 }
