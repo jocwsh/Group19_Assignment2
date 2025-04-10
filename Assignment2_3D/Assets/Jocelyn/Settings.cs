f1dfe4261b859ff097afefc1995e45c8e868d7ec
jocwsh <jojolyn.wal@gmail.com>
2025-04-11 01:47:09 +1000

    made settings done

--- Assignment2_3D/Assets/Jocelyn/Settings.cs
+++ Assignment2_3D/Assets/Jocelyn/Settings.cs
@@ -1,13 +1,59 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
 
 public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+
+
+    }
 
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("Volume", volume);
     }
 
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 }
