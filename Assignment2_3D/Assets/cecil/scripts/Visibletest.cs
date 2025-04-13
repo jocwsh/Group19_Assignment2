d3d9668a5a2cce0b9f58b3b3bcf755d68998e514
Dreamerunnamed <152683137+Dreamerunnamed@users.noreply.github.com>
2025-04-13 15:59:28 +1000

    added the evil cube
    added materials

---
+++ Assignment2_3D/Assets/cecil/scripts/Visibletest.cs
@@ -0,0 +1,5 @@
+// Hide button
+GameObject.Find ("ShareButton").transform.localScale = new Vector3(0, 0, 0);
+
+// Show button
+GameObject.Find ("ShareButton").transform.localScale = new Vector3(1, 1, 1);
