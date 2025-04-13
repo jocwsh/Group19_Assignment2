27ca169c5a635d2334159a1bf52b8c27e14bf940
Dreamerunnamed <152683137+Dreamerunnamed@users.noreply.github.com>
2025-04-13 19:48:58 +1000

    Removed speech bubble
    chose a different task

--- Assignment2_3D/Assets/cecil/scripts/Visibletest.cs
+++
@@ -1,5 +0,0 @@
-// Hide button
-GameObject.Find ("ShareButton").transform.localScale = new Vector3(0, 0, 0);
-
-// Show button
-GameObject.Find ("ShareButton").transform.localScale = new Vector3(1, 1, 1);
