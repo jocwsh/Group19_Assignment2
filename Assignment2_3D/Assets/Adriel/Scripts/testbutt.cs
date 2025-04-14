08b4d2843891dc927ee8a07d382c247a479b55c4
elsomoose <113397833+elsomoose@users.noreply.github.com>
2025-04-14 23:47:06 +1000

    testing

---
+++ Assignment2_3D/Assets/Adriel/Scripts/testbutt.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class testbutt : MonoBehaviour
+{
+
+    public GameObject player;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
